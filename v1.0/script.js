146e9404ce3fa3757e6d98be2f64ca4f25f0ed42
RB200 <66707944+RB200@users.noreply.github.com>
2022-11-04 16:59:01 -0700

    Add files via upload

---
+++ v1.0/script.js
@@ -0,0 +1,132 @@
+setTimeout(()=>{
+    var div = document.getElementsByClassName('component-heading')[1]
+
+    if(div){
+        var dataDiv = document.createElement('div')
+        dataDiv.setAttribute('id','data-div')
+        var total = 0
+        
+        var finalWithAdvisory = 0
+        var final = 0
+        let gradeList = []
+    
+        var grade_values = document.querySelectorAll('.numeric-grade')
+        var grade_arr = Array.from(grade_values)
+        
+        for(grade of grade_arr){
+            gradeList.push(grade.innerHTML)
+        }
+    
+        for(grade of gradeList){
+            total += parseFloat(grade)
+        }
+        
+        finalWithAdvisory = Math.round(total / (gradeList.length - 1) * 100) / 100
+        final = Math.round(total / (gradeList.length - 2) * 100) / 100
+
+        var letterWithoutAdvisory = getLetterGrade(final)
+        var letterWithAdvisory = getLetterGrade(finalWithAdvisory)
+        var dataTextWithAdvisory = document.createElement('h1')
+        dataTextWithAdvisory.innerHTML = `Average Grade:&ensp;${letterWithAdvisory}&emsp;&ensp;${finalWithAdvisory}% (includes advisory)`
+        var dataTextWithoutAdvisory = document.createElement('h1')
+        dataTextWithoutAdvisory.innerHTML = `Average Grade:&ensp;${letterWithoutAdvisory}&emsp;&ensp;${final}% (excludes advisory)`
+        dataDiv.style.fontSize = '12px'
+        dataDiv.style.textAlign = 'center'
+        dataDiv.style.justifyContent = 'center'
+        dataDiv.style.borderBottom = '1px solid #d6d6d6'
+        
+        dataTextWithAdvisory.style.fontWeight = '300'
+        dataTextWithoutAdvisory.style.fontWeight = '300'
+        dataDiv.appendChild(dataTextWithAdvisory)
+        dataDiv.appendChild(dataTextWithoutAdvisory)  
+        div.appendChild(dataDiv)
+    }
+    else{
+        var div = document.getElementsByClassName('vx-list course-list active')[0]
+        var dataDiv = document.createElement('div')
+        dataDiv.setAttribute('id','data-div')
+
+        var total = 0
+        
+        var finalWithAdvisory = 0
+        var final = 0
+        let gradeList = []
+    
+        var grade_values = document.querySelectorAll('.course-numeric-grade')
+        var grade_arr = Array.from(grade_values)
+        
+        for(grade of grade_arr){
+            gradeList.push(grade.innerHTML)
+        }
+        
+
+        for(grade of gradeList){
+            total += parseFloat(grade)
+        }
+        gradeList.pop()
+        gradeList.pop()
+        finalWithAdvisory = Math.round(total / (gradeList.length - 1) * 100) / 100
+        final = Math.round(total / (gradeList.length - 2) * 100) / 100
+        
+        var letterWithoutAdvisory = getLetterGrade(final)
+        var letterWithAdvisory = getLetterGrade(finalWithAdvisory)
+        var dataTextWithAdvisory = document.createElement('h1')
+        dataTextWithAdvisory.innerHTML = `Average Grade:&ensp;${letterWithAdvisory}&emsp;&ensp;${finalWithAdvisory}% (includes advisory)`
+        var dataTextWithoutAdvisory = document.createElement('h1')
+        dataTextWithoutAdvisory.innerHTML = `Average Grade:&ensp;${letterWithoutAdvisory}&emsp;&ensp;${final}% (excludes advisory)`
+        dataDiv.style.fontSize = '12px'
+        dataDiv.style.textAlign = 'center'
+        dataDiv.style.justifyContent = 'center'
+        dataDiv.style.borderBottom = '1px solid #d6d6d6'
+        
+        dataTextWithAdvisory.style.fontWeight = '300'
+        dataTextWithoutAdvisory.style.fontWeight = '300'
+        
+        dataDiv.appendChild(dataTextWithAdvisory)
+        dataDiv.appendChild(dataTextWithoutAdvisory)
+        div.prepend(dataDiv)
+    }
+    
+
+    function getLetterGrade(num){
+        var letterGrade = ''
+        if(num >= 92.50){
+            letterGrade = 'A'
+        }
+        else if(num >= 90.00 && num < 92.50){
+            letterGrade = 'A-'
+        }
+        else if(num >= 87.00 && num < 90.00){
+            letterGrade = 'B+'
+        }
+        else if(num >= 83.00 && num < 87.00){
+            letterGrade = 'B'
+        }
+        else if(num >= 80.00 && num < 83.00){
+            letterGrade = 'B-'
+        }
+        else if(num >= 77.00 && num < 80.00){
+            letterGrade = 'C+'
+        }
+        else if(num >= 73.00 && num < 77.00){
+            letterGrade = 'C'
+        }
+        else if(num >= 70.00 && num < 73.00){
+            letterGrade = 'C-'
+        }
+        else if(num >= 67.00 && num < 70){
+            letterGrade = 'D+'
+        }
+        else if(num >= 63.00 && num < 67.00){
+            letterGrade = 'D'
+        } 
+        else if(num >= 60.00 && num < 63.00){
+            letterGrade = 'D-'
+        }
+        else{
+            letterGrade = 'F'
+        }
+
+        return letterGrade
+    }
+},200)
